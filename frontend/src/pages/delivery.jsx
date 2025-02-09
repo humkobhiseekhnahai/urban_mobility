604a0c2376f61a51eb65676939d53f79fed83fef
Pratham Sharma <root1@Prathams-MacBook-Air.local>
2025-02-10 01:19:30 +0530

    delivery page completed

--- frontend/src/pages/delivery.jsx
+++ frontend/src/pages/delivery.jsx
@@ -1,36 +1,44 @@
-import RouteInput from "../components/routeInput"
+// Delivery.js
+import RouteInput from "../components/deliveryComponets/routeInput"
 import { NavBar } from "../components/navBar"
-import DeliveryMap from "../components/deliveryMap"
-
+import DeliveryMap from "../components/deliveryComponets/deliveryMap"
+import { Inventory } from "../components/deliveryComponets/inventory"
 
 export const Delivery = () => {
   return (
-    <div className="min-w-screen min-h-screen h-full bg-neutral-900 overflow-x-hidden">
-      <div className="max-w-6xl min-h-screen mx-auto">
-        {/* navbar */}
+    <div className="w-screen min-h-screen bg-neutral-900 overflow-x-hidden">
+      <div className="max-w-6xl mx-auto min-h-screen w-full px-4">
         <NavBar />
 
-        {/* map */}
-        <div className="flex justify-evenly m-5 h-fit">
+        {/* Map Section */}
+        <div className="flex flex-col md:flex-row justify-between items-stretch m-5 gap-4">
           <RouteInput />
-          <div className="text-white border-2 h-fit w-full">
-            <DeliveryMap/>
+          <div className="text-white border-2 flex-1 w-full">
+            <DeliveryMap />
           </div>
         </div>
 
-        {/* inputs */}
-
+        {/* Inputs */}
         <div className="flex justify-center mt-10">
           <input
-            placeholder="Enter vehicle type*"
-            className="text-white text-center border-2 p-2 m-2 mr-5"
+            disabled
+            placeholder="Sample Vehicle"
+            className="text-white text-center border-2 p-2 m-2 mr-5 cursor-not-allowed"
           />
           <input
+            type="number"
             placeholder="Enter capacity*"
             className="text-white text-center border-2 p-2 m-2"
           />
         </div>
 
+        <Inventory />
+        
+        <div className="flex justify-center items-center m-2">
+          <button className="text-white bg-blue-500 py-3 px-20 rounded-md">
+            OPTIMIZE
+          </button>
+        </div>
       </div>
     </div>
   )
