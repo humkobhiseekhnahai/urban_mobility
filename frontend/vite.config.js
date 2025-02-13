671219118f94f1def03c54d4c65d697c62f4c4b1
Pratham Sharma <113964544+humkobhiseekhnahai@users.noreply.github.com>
2025-02-13 22:12:17 +0530

    Revert "City dashboard"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import ReactCompiler from "babel-plugin-react-compiler";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [ReactCompiler],
-      },
-    }),
-    tailwindcss(),
+    react(),
+    tailwindcss()
   ],
-});
+})
