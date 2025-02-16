0ed2dd326a750430619932b5a790e4f759f85f9b
amanraj1105 <aman1105raj@gmail.com>
2025-02-16 20:58:04 +0530

    testing

--- frontend/src/components/homeComponents/navBar_home.jsx
+++ frontend/src/components/homeComponents/navBar_home.jsx
@@ -27,15 +27,15 @@ const Navbar_home = () => {
             ))}
           </ul>
           <div className="hidden lg:flex justify-center space-x-12 items-center">
-            <a href="#" className="py-2 px-3 border rounded-md">
-              Sign In
+            <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md">
+              Sign In/Register
             </a>
-            <a
+            {/* <a
               href="#"
               className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
             >
               Create an account
-            </a>
+            </a> */}
           </div>
           <div className="lg:hidden md:flex flex-col justify-end">
           <button onClick={toggleNavbar}>
@@ -54,15 +54,15 @@ const Navbar_home = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <a href="#" className="py-2 px-3 border rounded-md">
-                Sign In
+              <a href="#" className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800">
+                Sign In/Register
               </a>
-              <a
+              {/* <a
                 href="#"
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
                 Create an account
-              </a>
+              </a> */}
             </div>
           </div>
         )}
