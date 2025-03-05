95f700a2da60da4029a6a5e7ab77329b86dcb04e
Abhinav <abhinavsingh1632@gmail.com>
2025-03-05 22:36:49 +0530

    Migration added 3/5

---
+++ node_backend/prisma/migrations/20250304161531_bglr_db/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `agency_fare_url` on the `Agency` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Agency" DROP COLUMN "agency_fare_url";
