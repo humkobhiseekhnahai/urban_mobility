f2d7ef799ce6d46848f7bf7914631e47941d86b6
Pratham Sharma <root1@Prathams-MacBook-Air.local>
2025-03-05 06:47:18 +0530

    added deliveryMap

--- frontend/src/hooks/atoms/atom.jsx
+++ frontend/src/hooks/atoms/atom.jsx
@@ -1,19 +1,22 @@
-// atoms.js (updated for Jotai)
+// atoms.js
 import { atom } from 'jotai';
 
-export const inputAtom = atom([{ location: "", capacity: 0, priority:"low"}]);
-
-export const markerAtom = atom((get) => {
-    const input = get(inputAtom);
-    return input
-        .filter(stop => stop.location)
-        .map(stop => 
-            stop.location.split(',')
-                .map(num => parseFloat(num.trim()))
-                .filter(num => !isNaN(num))
-        )
-        .filter(coords => coords.length === 2);
-});
-
+export const startingLocationAtom = atom("");
+export const deliveryStopsAtom = atom([]);
 export const apiResponseAtom = atom(null);
 
+export const markerAtom = atom((get) => {
+  const start = get(startingLocationAtom);
+  const stops = get(deliveryStopsAtom);
+  
+  const allLocations = [
+    start,
+    ...stops.map(stop => stop.location)
+  ].filter(location => location);
+
+  return allLocations.map(location => 
+    location.split(',')
+      .map(num => parseFloat(num.trim()))
+      .filter(num => !isNaN(num))
+  ).filter(coords => coords.length === 2);
+});
