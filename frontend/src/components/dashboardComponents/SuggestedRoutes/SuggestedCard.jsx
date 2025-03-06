9e312bf73eb0c766bb9491cfeb13bf26c25ae0ed
Kabeer Arora <kabeerarora13@gmail.com>
2025-03-06 16:42:52 +0530

    feat: Dashboard revamped

--- frontend/src/components/dashboardComponents/SuggestedRoutes/SuggestedCard.jsx
+++ frontend/src/components/dashboardComponents/SuggestedRoutes/SuggestedCard.jsx
@@ -6,13 +6,45 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import { MapPin, MapPinCheckInside } from "lucide-react";
+import { routeAtom } from "../../../hooks/atoms/atom";
 
-export const SuggestedCard = () => {
+export const SuggestedCard = ({ route }) => {
+  const parsedRoute = {
+    source: route.source,
+    destination: route.destination,
+    coordinates: JSON.parse(route.coordinates),
+  };
+
   return (
-    <Card className="mt-6 w-96">
-      <CardHeader color="blue-gray" className="relative h-56"></CardHeader>
-      <CardBody></CardBody>
-      <CardFooter className="pt-0"></CardFooter>
+    <Card className="w-full">
+      <CardBody className="flex flex-col items-center">
+        <div className="w-full flex justify-between">
+          {/* Source */}
+          <Typography variant="h6" color="gray" className="flex items-center">
+            <span className="pr-1">
+              <MapPin size={16} />
+            </span>
+            {route.source}
+          </Typography>
+          {/* Desination */}
+          <Typography variant="h6" color="gray" className="flex items-center">
+            <span className="pr-1">
+              <MapPinCheckInside size={16} />
+            </span>
+            {route.destination}
+          </Typography>
+        </div>
+      </CardBody>
+      <CardFooter className="flex justify-end">
+        <Button
+          variant="gradient"
+          className="mt-4"
+          onClick={() => routeAtom(parsedRoute)}
+        >
+          View Route
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
--- frontend/src/components/dashboardComponents/SuggestedRoutes/SuggestedList.jsx
+++ frontend/src/components/dashboardComponents/SuggestedRoutes/SuggestedList.jsx
@@ -6,30 +6,14 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import { SuggestedCard } from "./SuggestedCard";
 
-const routeData = [
-  {
-    routeName: "Downtown to Airport",
-    distance: "12.5 km",
-    duration: "25 mins",
-    traffic: "Moderate",
-    startingPoint: "Downtown Center",
-    destination: "International Airport",
-  },
-  {
-    routeName: "City Center to University",
-    distance: "8.2 km",
-    duration: "18 mins",
-    traffic: "Heavy",
-    startingPoint: "City Center",
-    destination: "State University",
-  },
-  {
-    routeName: "Mall to Tech Park",
-    distance: "15 km",
-    duration: "30 mins",
-    traffic: "Light",
-    startingPoint: "Central Mall",
-    destination: "Tech Park",
-  },
-];
+export const SuggestedList = ({ routes }) => {
+  return (
+    <div className="w-full flex flex-col space-y-4 items-center">
+      {routes.map((route) => {
+        return <SuggestedCard route={route} />;
+      })}
+    </div>
+  );
+};
