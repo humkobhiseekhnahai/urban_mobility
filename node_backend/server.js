13a6110a47957d08ee0987276e7a673365274a33
Pratham Sharma <root1@Prathams-MacBook-Air.local>
2025-03-12 23:34:40 +0530

    updating the serverjs

--- node_backend/server.js
+++ node_backend/server.js
@@ -148,7 +148,7 @@ app.get("/api/bus-routes/:routeNumber", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
